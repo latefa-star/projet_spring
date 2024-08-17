d7ff800c7ba7956f4c00cb1898961d46e3841e5c
InfoDz125 <latifa.bouchekif96@gmail.com>
2024-08-17 23:51:46 +0200

    add test for get request

--- src/test/java/com/example/test/TodoControllerTest.java
+++ src/test/java/com/example/test/TodoControllerTest.java
@@ -1,15 +1,54 @@
 package com.example.test;
 
 
+import com.example.test.entities.Todo;
+import com.example.test.service.ToDoService;
+import com.jayway.jsonpath.JsonPath;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class TodoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private ToDoService todoService;
+
+
+
+    @Test
+    public void GetAllTodoTest() throws Exception
+    {
+        Todo t1=new Todo("1","Titre1","desc1");
+        Todo t2=new Todo("2","Titre2","desc2");
+
+        List<Todo> fakeList= Arrays.asList(t1,t2);
+        given(todoService.listsTodo()).willReturn(fakeList);
+
+        mvc.perform(MockMvcRequestBuilders.get("/todo/api/").
+                contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)));
+
+    }
 
 
 }
